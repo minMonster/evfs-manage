d77296f865d0974185cd3e40a28e7d229b86e439
monster <monster.lixuan@ksher.net>
2020-10-09 23:27:01 +0800

    feat: build 业务域名 待审批

---
+++ src/views/admin/business/audit/contract-copyy.vue
@@ -0,0 +1,367 @@
+<template>
+  <div class="business-detail">
+    <div class="content-title"><span>业务域内合约治理</span></div>
+    <div class="bg-white padding">
+      <div class="section-title">
+        <span>
+          <b>文件监管</b>
+        </span>
+      </div>
+      <div>
+        <Table :columns="columns1" :data="data1"></Table>
+      </div>
+      <div class="page">
+        <div class="page-inner">
+          <Page :total="total" @on-change="pageChange" />
+        </div>
+      </div>
+    </div>
+    <div class="bg-white padding mb20" style="margin-bottom:20px;">
+      <div style="margin-bottom: 10px;color: #273D52;">
+        <span>
+          <b>结构数据监管</b>
+        </span>
+      </div>
+      <div>
+        <Table :columns="columns2" :data="data2"></Table>
+      </div>
+      <div class="page">
+        <div class="page-inner">
+          <Page :total="total" @on-change="pageChange" />
+        </div>
+      </div>
+    </div>
+    <div class="bg-white padding mb20">
+      <div style="margin-bottom: 10px;color: #273D52;">
+        <span>
+          <b>文件拥有者转移待审列表</b>
+        </span>
+      </div>
+      <div style="overflow: auto;">
+        <Table style="min-width: 1300px;" :columns="columns3" :data="data3"></Table>
+      </div>
+      <div class="page">
+        <div class="page-inner">
+          <Page :total="total" @on-change="pageChange" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    let that = this
+    let columns1 = [
+      {
+        title: '合约名称',
+        key: 'name'
+      },
+      {
+        title: '合约链上唯一标识',
+        key: 'address'
+      },
+      {
+        title: '发布时间',
+        key: 'time'
+      },
+      {
+        title: '状态',
+        key: 'statuslabel'
+      },
+      {
+        width: 120,
+        title: '申请人',
+        key: 'applicant'
+      },
+      {
+        width: 130,
+        title: '审核通过人',
+        render (h, p) {
+          let row = p.row
+          return h('a', {
+            on: {
+              click () {
+                that.adds(row)
+              }
+            }
+          }, '查看')
+        }
+      },
+      {
+        title: '操作',
+        render (h, p) {
+          let agree = h('a', {
+            style: {
+              marginRight: '8px'
+            },
+            domProps: {
+              href: 'javascript:;'
+            },
+            on: {
+              click () {
+                // let index = p.index
+              }
+            }
+          }, '同意')
+          let refuse = h('a', {
+            domProps: {
+              href: 'javascript:;'
+            },
+            on: {
+              click () {
+                // let index = p.index
+              }
+            }
+          }, '拒绝')
+          return h('div', {
+            'class': 'opt-btns'
+          }, [
+            agree, refuse
+          ])
+        }
+      }
+    ]
+    let data1 = [
+      { name: '电子卷宗管理', address: '00fb0a...eac32', applicant: '张力', time: '--', statuslabel: '发布审核中', status: '0' },
+      { name: '银行贷款信息', address: '00fb0a...dfa86', applicant: '张力', time: '2020-4-20 13:10:09', statuslabel: '解冻审核中', status: '1' },
+      { name: '公证书管理', address: '00fb0a...adee2', applicant: '张力', time: '2020-4-20 15:22:24', statuslabel: '冻结审核中', status: '2' },
+      { name: '执行管理', address: '00fb0a...ffe76', applicant: '张力', time: '2020-4-20 15:22:24', statuslabel: '解冻审核中', status: '4' },
+      { name: '仲裁管理', address: '00fb0a...efcca', applicant: '张力', time: '2020-4-20 14:21:05', statuslabel: '冻结审核中', status: '3' }
+    ]
+    let columns2 = [
+      {
+        title: '数据链上唯一标识',
+        key: 'address'
+      },
+      {
+        title: '状态',
+        key: 'statuslabel'
+      },
+      {
+        width: 120,
+        title: '申请人',
+        key: 'applicant'
+      },
+      {
+        width: 130,
+        title: '审核通过人',
+        render (h, p) {
+          let row = p.row
+          return h('a', {
+            on: {
+              click () {
+                that.addlook(row)
+              }
+            }
+          }, '查看')
+        }
+      },
+      {
+        title: '操作',
+        render (h, p) {
+          let agree = h('a', {
+            style: {
+              marginRight: '8px'
+            },
+            domProps: {
+              href: 'javascript:;'
+            },
+            on: {
+              click () {
+                // let index = p.index
+              }
+            }
+          }, '同意')
+          let refuse = h('a', {
+            domProps: {
+              href: 'javascript:;'
+            },
+            on: {
+              click () {
+                // let index = p.index
+              }
+            }
+          }, '拒绝')
+          return h('div', {
+            'class': 'opt-btns'
+          }, [
+            agree, refuse
+          ])
+        }
+      },
+      {
+        title: '审核结果',
+        render (h, p) {
+          let agree = h('a', {
+            style: {
+              marginRight: '8px'
+            },
+            domProps: {
+              href: 'javascript:;'
+            },
+            on: {
+              click () {
+                // let index = p.index
+              }
+            }
+          }, '同意')
+          let refuse = h('a', {
+            domProps: {
+              href: 'javascript:;'
+            },
+            on: {
+              click () {
+                // let index = p.index
+              }
+            }
+          }, '拒绝')
+          return h('div', {
+            'class': 'opt-btns'
+          }, [
+            agree, refuse
+          ])
+        }
+      }
+    ]
+    let data2 = [
+      { address: '00fb0a...ef34a', applicant: '张力', statuslabel: '解冻审核中', status: '1' },
+      { address: '00fb0a...eda4e', applicant: '张力', statuslabel: '冻结审核中', status: '2' },
+      { address: '00fb0a...eac4f', applicant: '张力', statuslabel: '解冻审核中', status: '4' },
+      { address: '00fb0a...a43fe', applicant: '张力', statuslabel: '冻结审核中', status: '3' }
+    ]
+    let columns3 = [
+      {
+        title: '文件名称',
+        key: 'name'
+      },
+      {
+        title: '文件链上唯一标识',
+        key: 'address'
+      },
+      {
+        title: '原始拥有者姓名',
+        key: 'oldname'
+      },
+      {
+        title: '原始拥有者身份标识',
+        key: 'oldid'
+      },
+      {
+        title: '新拥有者姓名',
+        key: 'newname'
+      },
+      {
+        title: '新拥有者身份标识',
+        key: 'newid'
+      },
+      {
+        width: 130,
+        title: '审核通过人',
+        render (h, p) {
+          let row = p.row
+          return h('a', {
+            on: {
+              click () {
+                that.addLook(row)
+              }
+            }
+          }, '查看')
+        }
+      },
+      {
+        title: '操作',
+        render (h, p) {
+          let agree = h('a', {
+            style: {
+              marginRight: '8px'
+            },
+            domProps: {
+              href: 'javascript:;'
+            },
+            on: {
+              click () {
+                // let index = p.index
+              }
+            }
+          }, '同意')
+          let refuse = h('a', {
+            domProps: {
+              href: 'javascript:;'
+            },
+            on: {
+              click () {
+                // let index = p.index
+              }
+            }
+          }, '拒绝')
+          return h('div', {
+            'class': 'opt-btns'
+          }, [
+            agree, refuse
+          ])
+        }
+      }
+    ]
+    let data3 = [
+      {
+        name: '测试122.doc',
+        address: '00fb0a...ef34a',
+        oldname: '张力',
+        oldid: '00cc0b...ae7ca',
+        newname: '赵宇',
+        newid: '00cc0b...af2c4'
+      }
+    ]
+    return {
+      total: 90,
+      columns1,
+      data1,
+      columns2,
+      data2,
+      columns3,
+      data3
+    }
+  },
+  watch: {},
+  computed: {},
+  mounted () {
+    this.init()
+  },
+  methods: {
+    init () {
+
+    },
+    // 查看
+    adds (obj) {
+      this.$Modal.confirm({
+        title: '已审核人列表',
+        content: 'name：' + obj.name + '<br> address：' + obj.address + ' <br>time：' + obj.time + '',
+        oktext: '关闭'
+      })
+    },
+    addLook (obj) {
+      this.$Modal.confirm({
+        title: '已审核人列表',
+        content: 'name：' + obj.name + '<br> address：' + obj.address + '',
+        oktext: '关闭'
+      })
+    },
+    addlook (obj) {
+      this.$Modal.confirm({
+        title: '已审核人列表',
+        content: 'name：' + obj.applicant + '<br> address：' + obj.address + '',
+        oktext: '关闭'
+      })
+    },
+    pageChange (val) {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/admin/business/audit/contract.vue
+++ src/views/admin/business/audit/contract.vue
@@ -4,45 +4,20 @@
     <div class="bg-white padding">
       <div class="section-title">
         <span>
-          <b>文件监管</b>
+          <b>合约列表</b>
         </span>
       </div>
       <div>
-        <Table :columns="columns1" :data="data1"></Table>
+        <Table :columns="columns" :loading="listLoading" :data="list"></Table>
       </div>
       <div class="page">
         <div class="page-inner">
-          <Page :total="total" @on-change="pageChange" />
-        </div>
-      </div>
-    </div>
-    <div class="bg-white padding mb20" style="margin-bottom:20px;">
-      <div style="margin-bottom: 10px;color: #273D52;">
-        <span>
-          <b>结构数据监管</b>
-        </span>
-      </div>
-      <div>
-        <Table :columns="columns2" :data="data2"></Table>
-      </div>
-      <div class="page">
-        <div class="page-inner">
-          <Page :total="total" @on-change="pageChange" />
-        </div>
-      </div>
-    </div>
-    <div class="bg-white padding mb20">
-      <div style="margin-bottom: 10px;color: #273D52;">
-        <span>
-          <b>文件拥有者转移待审列表</b>
-        </span>
-      </div>
-      <div style="overflow: auto;">
-        <Table style="min-width: 1300px;" :columns="columns3" :data="data3"></Table>
-      </div>
-      <div class="page">
-        <div class="page-inner">
-          <Page :total="total" @on-change="pageChange" />
+          <Page
+            show-sizer
+            :total="page.total"
+            :current="page.current"
+            @on-change="pageChange"
+            @on-page-size-change="sizeChange"/>
         </div>
       </div>
     </div>
@@ -50,10 +25,12 @@
 </template>
 
 <script>
+import * as api from '../api'
+import * as cApi from '@/http/api'
 export default {
   data () {
     let that = this
-    let columns1 = [
+    let columns = [
       {
         title: '合约名称',
         key: 'name'
@@ -64,16 +41,24 @@ export default {
       },
       {
         title: '发布时间',
-        key: 'time'
+        key: 'join_time',
+        render (h, p) {
+          let row = p.row
+          if (!row.join_time) {
+            return h('span', '--')
+          } else {
+            return h('span', that.$moment.unix(row.join_time / 1000).format('YYYY-MM-DD HH:mm:ss'))
+          }
+        }
       },
       {
         title: '状态',
-        key: 'statuslabel'
+        key: 'status'
       },
       {
         width: 120,
         title: '申请人',
-        key: 'applicant'
+        key: 'applicant_name'
       },
       {
         width: 130,
@@ -91,76 +76,9 @@ export default {
       },
       {
         title: '操作',
-        render (h, p) {
-          let agree = h('a', {
-            style: {
-              marginRight: '8px'
-            },
-            domProps: {
-              href: 'javascript:;'
-            },
-            on: {
-              click () {
-                // let index = p.index
-              }
-            }
-          }, '同意')
-          let refuse = h('a', {
-            domProps: {
-              href: 'javascript:;'
-            },
-            on: {
-              click () {
-                // let index = p.index
-              }
-            }
-          }, '拒绝')
-          return h('div', {
-            'class': 'opt-btns'
-          }, [
-            agree, refuse
-          ])
-        }
-      }
-    ]
-    let data1 = [
-      { name: '电子卷宗管理', address: '00fb0a...eac32', applicant: '张力', time: '--', statuslabel: '发布审核中', status: '0' },
-      { name: '银行贷款信息', address: '00fb0a...dfa86', applicant: '张力', time: '2020-4-20 13:10:09', statuslabel: '解冻审核中', status: '1' },
-      { name: '公证书管理', address: '00fb0a...adee2', applicant: '张力', time: '2020-4-20 15:22:24', statuslabel: '冻结审核中', status: '2' },
-      { name: '执行管理', address: '00fb0a...ffe76', applicant: '张力', time: '2020-4-20 15:22:24', statuslabel: '解冻审核中', status: '4' },
-      { name: '仲裁管理', address: '00fb0a...efcca', applicant: '张力', time: '2020-4-20 14:21:05', statuslabel: '冻结审核中', status: '3' }
-    ]
-    let columns2 = [
-      {
-        title: '数据链上唯一标识',
-        key: 'address'
-      },
-      {
-        title: '状态',
-        key: 'statuslabel'
-      },
-      {
-        width: 120,
-        title: '申请人',
-        key: 'applicant'
-      },
-      {
-        width: 130,
-        title: '审核通过人',
+        'width': 120,
         render (h, p) {
           let row = p.row
-          return h('a', {
-            on: {
-              click () {
-                that.addlook(row)
-              }
-            }
-          }, '查看')
-        }
-      },
-      {
-        title: '操作',
-        render (h, p) {
           let agree = h('a', {
             style: {
               marginRight: '8px'
@@ -171,6 +89,7 @@ export default {
             on: {
               click () {
                 // let index = p.index
+                that.agree(row)
               }
             }
           }, '同意')
@@ -180,6 +99,7 @@ export default {
             },
             on: {
               click () {
+                that.refuse(row)
                 // let index = p.index
               }
             }
@@ -190,138 +110,26 @@ export default {
             agree, refuse
           ])
         }
-      },
-      {
-        title: '审核结果',
-        render (h, p) {
-          let agree = h('a', {
-            style: {
-              marginRight: '8px'
-            },
-            domProps: {
-              href: 'javascript:;'
-            },
-            on: {
-              click () {
-                // let index = p.index
-              }
-            }
-          }, '同意')
-          let refuse = h('a', {
-            domProps: {
-              href: 'javascript:;'
-            },
-            on: {
-              click () {
-                // let index = p.index
-              }
-            }
-          }, '拒绝')
-          return h('div', {
-            'class': 'opt-btns'
-          }, [
-            agree, refuse
-          ])
-        }
-      }
-    ]
-    let data2 = [
-      { address: '00fb0a...ef34a', applicant: '张力', statuslabel: '解冻审核中', status: '1' },
-      { address: '00fb0a...eda4e', applicant: '张力', statuslabel: '冻结审核中', status: '2' },
-      { address: '00fb0a...eac4f', applicant: '张力', statuslabel: '解冻审核中', status: '4' },
-      { address: '00fb0a...a43fe', applicant: '张力', statuslabel: '冻结审核中', status: '3' }
-    ]
-    let columns3 = [
-      {
-        title: '文件名称',
-        key: 'name'
-      },
-      {
-        title: '文件链上唯一标识',
-        key: 'address'
-      },
-      {
-        title: '原始拥有者姓名',
-        key: 'oldname'
-      },
-      {
-        title: '原始拥有者身份标识',
-        key: 'oldid'
-      },
-      {
-        title: '新拥有者姓名',
-        key: 'newname'
-      },
-      {
-        title: '新拥有者身份标识',
-        key: 'newid'
-      },
-      {
-        width: 130,
-        title: '审核通过人',
-        render (h, p) {
-          let row = p.row
-          return h('a', {
-            on: {
-              click () {
-                that.addLook(row)
-              }
-            }
-          }, '查看')
-        }
-      },
-      {
-        title: '操作',
-        render (h, p) {
-          let agree = h('a', {
-            style: {
-              marginRight: '8px'
-            },
-            domProps: {
-              href: 'javascript:;'
-            },
-            on: {
-              click () {
-                // let index = p.index
-              }
-            }
-          }, '同意')
-          let refuse = h('a', {
-            domProps: {
-              href: 'javascript:;'
-            },
-            on: {
-              click () {
-                // let index = p.index
-              }
-            }
-          }, '拒绝')
-          return h('div', {
-            'class': 'opt-btns'
-          }, [
-            agree, refuse
-          ])
-        }
-      }
-    ]
-    let data3 = [
-      {
-        name: '测试122.doc',
-        address: '00fb0a...ef34a',
-        oldname: '张力',
-        oldid: '00cc0b...ae7ca',
-        newname: '赵宇',
-        newid: '00cc0b...af2c4'
       }
     ]
     return {
-      total: 90,
-      columns1,
-      data1,
-      columns2,
-      data2,
-      columns3,
-      data3
+      listLoading: false,
+      columns,
+      oldList: [
+        // {
+        //   'member_id': 1,
+        //   'member_address': '1',
+        //   'main_committeegroup_group_id': '1',
+        //   'join_time': 1598345923000,
+        //   'member_name': '名称'
+        // }
+      ],
+      list: [],
+      page: {
+        total: 1,
+        current: 1,
+        size: 10
+      }
     }
   },
   watch: {},
@@ -331,32 +139,129 @@ export default {
   },
   methods: {
     init () {
-
-    },
-    // 查看
-    adds (obj) {
-      this.$Modal.confirm({
-        title: '已审核人列表',
-        content: 'name：' + obj.name + '<br> address：' + obj.address + ' <br>time：' + obj.time + '',
-        oktext: '关闭'
+      this.listLoading = true
+      api.pbqrc({
+        'menu': 'biz',
+        reviewType: 'biz_contract',
+        address: sessionStorage.getItem('fbs_address')
+      }).then(res => {
+        this.listLoading = false
+        this.oldList = res.rows
+        this.page.total = this.oldList.length
+        this.getList()
+      }).catch(err => {
+        this.listLoading = false
+        this.$Message.error(err.retMsg)
       })
     },
-    addLook (obj) {
-      this.$Modal.confirm({
-        title: '已审核人列表',
-        content: 'name：' + obj.name + '<br> address：' + obj.address + '',
-        oktext: '关闭'
+    async agree (row) {
+      let jsBody = {
+        from: sessionStorage.getItem('fbs_address'),
+        reqId: row.review_id,
+        'domainId': sessionStorage.getItem('fbs_biz_id') // 业务域ID
+      }
+      let data = await cApi.pbgen({
+        'method': 'BizDomainCreateContractAgreeContractTxReq',
+        'jsBody': JSON.stringify(jsBody)
+      }).then(res => {
+        return {
+          hexTxBody: res.hexTxBody,
+          txId: res.txId
+        }
+      }).catch(err => {
+        this.$Message.error(err.retMsg)
+        return false
       })
+      if (data) {
+        this.$qrCodeAuthDialog.show(
+          {
+            url: 'bs/pbdtx.do',
+            data,
+            // 这里要写一个闭包函数 返回 需要的 api
+            setIntervalFunc: () => cApi.pbgts({ txId: data.txId }),
+            func: 'send_trans'
+          },
+          (resPromise) => {
+            // resPromise 轮询的结果 在此处处理业务逻辑
+            return resPromise.then(res => {
+              // 1待提交；2执行中；3执行完成；4执行失败；5提交失败；6未知状态
+              if (res.status === 4 || res.status === 5 || res.status === 6) {
+                this.$Message.error(res.remark)
+                return true
+              }
+              if (res.status === 3) {
+                this.$Message.success('修改成功')
+                this.addModal = false
+                return true
+              } else {
+                return false
+              }
+            }).catch(() => {
+              return false
+            })
+          })
+      }
     },
-    addlook (obj) {
-      this.$Modal.confirm({
-        title: '已审核人列表',
-        content: 'name：' + obj.applicant + '<br> address：' + obj.address + '',
-        oktext: '关闭'
+    async refuse (row) {
+      let jsBody = {
+        from: sessionStorage.getItem('fbs_address'),
+        reqId: row.review_id,
+        'domainId': sessionStorage.getItem('fbs_biz_id') // 业务域ID
+      }
+      let data = await cApi.pbgen({
+        'method': 'BizDomainDisagreeContractTxReq',
+        'jsBody': JSON.stringify(jsBody)
+      }).then(res => {
+        return {
+          hexTxBody: res.hexTxBody,
+          txId: res.txId
+        }
+      }).catch(err => {
+        this.$Message.error(err.retMsg)
+        return false
       })
+      if (data) {
+        this.$qrCodeAuthDialog.show(
+          {
+            url: 'bs/pbdtx.do',
+            data,
+            // 这里要写一个闭包函数 返回 需要的 api
+            setIntervalFunc: () => cApi.pbgts({ txId: data.txId }),
+            func: 'send_trans'
+          },
+          (resPromise) => {
+            // resPromise 轮询的结果 在此处处理业务逻辑
+            return resPromise.then(res => {
+              // 1待提交；2执行中；3执行完成；4执行失败；5提交失败；6未知状态
+              if (res.status === 4 || res.status === 5 || res.status === 6) {
+                this.$Message.error(res.remark)
+                return true
+              }
+              if (res.status === 3) {
+                this.$Message.success('修改成功')
+                this.addModal = false
+                return true
+              } else {
+                return false
+              }
+            }).catch(() => {
+              return false
+            })
+          })
+      }
     },
-    pageChange (val) {
-
+    getList () {
+      this.list = this.oldList.slice((this.page.current - 1) * this.page.size, this.page.size * this.page.current)
+    },
+    sizeChange (size) {
+      this.page.current = 1
+      this.page.size = size
+      this.getList()
+    },
+    // 分页
+    pageChange (page) {
+      this.page.current = page
+      this.getList()
     }
   }
 }
